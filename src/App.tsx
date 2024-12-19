9992109bed65ba096e4aab4ac3d8a2c2c035f717
greta <Djymini>
2024-12-19 10:51:27 +0100

    Card movie component created

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,30 @@ import Page from "./Manager/Page";
 import Header from "./components/Header";
 import MovieCard from "./components/Card";
 
+const test = {
+        "adult": false,
+        "backdrop_path": "/hziiv14OpD73u9gAak4XDDfBKa2.jpg",
+        "genre_ids": [
+            12,
+            14
+        ],
+        "id": 671,
+        "original_language": "en",
+        "original_title": "Harry Potter and the Philosopher's Stone",
+        "overview": "Orphelin, le jeune Harry Potter peut enfin quitter ses tyranniques oncle et tante Dursley lorsqu'un curieux messager lui révèle qu'il est un sorcier. À 11 ans, Harry va enfin pouvoir intégrer la légendaire école de sorcellerie de Poudlard, y trouver une famille digne de ce nom et des amis, développer ses dons, et préparer son glorieux avenir.",
+        "popularity": 182.532,
+        "poster_path": "/fbxQ44VRdM2PVzHSNajUseUteem.jpg",
+        "release_date": "2001-11-16",
+        "title": "Harry Potter à l'école des sorciers",
+        "video": false,
+        "vote_average": 7.909,
+        "vote_count": 27420
+    };
+
 function App() {
   return (
    <Page title={"Prototype"}>
-       <MovieCard/>
+       <MovieCard movie={test} />
    </Page>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import {FC} from 'react';
+import {MovieItem as MovieType} from "../types/MovieItem";
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SvgIcon from '@mui/material/SvgIcon';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 
-const MovieCard: FC<{}> = ({}) => {
+const MovieCard: FC<{movie:MovieType}> = ({movie}) => {
+    const image = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+    const backgroundImageCard = {
+        backgroundSize: 'cover',
+        backgroundImage: `url(${image})`
+    };
     return (
-        <div className="movie-card">
+        <div className="movie-card" style={backgroundImageCard}>
             <div className="movie-card-info">
-
+                <div className="movie-card-info-text">
+                    <h2>{movie.title}</h2>
+                    <p>{movie.release_date}</p>
+                </div>
+                <div className="movie-card-button-container">
+                    <IconButton aria-label="Example">
+                        <PlayCircleOutlineIcon style={{color:'#E0E1DD', fontSize: 32}}/>
+                    </IconButton>
+                </div>
             </div>
         </div>
     );
