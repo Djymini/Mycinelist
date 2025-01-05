b0422e4bc0e4cf0ce321f63aa8eeba96864980f7
greta <Djymini>
2025-01-05 16:42:25 +0100

    Reboot: Navbar and his style are make

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
 import './App.css';
 import Header from "./pages/a_header/Header";
+import {Helmet} from "react-helmet-async";
 
 
 function App() {
   return (
           <>
-              <Header />
+              <Helmet>
+                  <title>Mycinelist</title>
+                  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                  <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                  <link
+                      href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Goldman:wght@400;700&display=swap"
+                      rel="stylesheet"/>
+              </Helmet>
+              <Header/>
           </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import {FC} from 'react';
-import Searchbar from "./Searchbar";
 import Button from '@mui/material/Button';
 import LoginIcon from '@mui/icons-material/Login';
 import CreateIcon from '@mui/icons-material/Create';
+import SearchIcon from '@mui/icons-material/Search';
 import Stack from '@mui/material/Stack';
+import {NavButton, Search, SearchIconWrapper, StyledInputBase} from "../../_styles/navbarStyles";
+
 
 const Navbar: FC<{}> = ({}) => {
     return (
@@ -17,11 +19,19 @@ const Navbar: FC<{}> = ({}) => {
                     <a href="/">Tableau de bord</a>
                 </nav>
                 <div className="vertical-line" style={{borderLeft: '2px solid', height: '47px'}}></div>
-                <Searchbar/>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon/>
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Rechercher"
+                        inputProps={{'aria-label': 'search'}}
+                    />
+                </Search>
             </div>
             <Stack direction="row" spacing={2}>
-                <Button variant="outlined" startIcon={<CreateIcon/>}>S'inscrire</Button>
-                <Button variant="outlined" startIcon={<LoginIcon/>}>Se connecter</Button>
+                <NavButton variant="contained" startIcon={<CreateIcon/>}>S'inscrire</NavButton>
+                <NavButton variant="contained" startIcon={<LoginIcon/>}>Se connecter</NavButton>
             </Stack>
 
         </>
